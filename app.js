6f47a1a31c5a5277559c31370f15b8da3ae66775
Eugene Roy <box.rz.box@gmail.com>
2022-05-23 23:06:06 +0300

    done

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+
+    const data = JSON.stringify([
+      { id: 1, text: "Hello" },
+      { id: 2, text: "world" },
+    ]);
+
+    // res.setHeader("Content-Type", "text/plain");
+    //   res.setHeader("Content-Type", "text/html");
+    res.setHeader("Content-Type", "application/json");
+
+    //   res.write("<h1>Hello world</h1>");
+    res.end(data);
 });
 
 server.listen(PORT, "localhost", (err) => {
