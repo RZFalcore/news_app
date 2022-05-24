d19f0a4cfc3417643a73e5c7b61864face6340f7
Eugene Roy <box.rz.box@gmail.com>
2022-05-24 23:40:43 +0300

    added status codes

--- app.js
+++ app.js
@@ -14,18 +14,22 @@ const server = http.createServer((req, res) => {
   switch (req.url) {
     case "/":
       basePath = createPath("index");
+      res.statusCode = 200;
       break;
-    case "/contacts":
-      basePath = createPath("contacts");
-      break;
-    default:
-      basePath = createPath("error");
+      case "/contacts":
+        basePath = createPath("contacts");
+        res.statusCode = 200;
+        break;
+        default:
+          basePath = createPath("error");
+          res.statusCode = 404;
       break;
   }
 
   fs.readFile(basePath, (err, data) => {
     if (err) {
       console.log(err);
+      res.statusCode = 500;
       res.end();
     } else {
       res.write(data);
